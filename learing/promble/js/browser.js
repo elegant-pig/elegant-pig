ea76e0d7889799b9851335db34f0c3f565827749
elegant-pig <60571427+elegant-pig@users.noreply.github.com>
2022-03-12 20:26:55 +0800

    20210725

---
+++ learing/promble/js/browser.js
@@ -0,0 +1,12 @@
+//禁用浏览器前进后退功能
+		$(document).ready(function () {
+		                  if (window.history && window.history.pushState) {
+		                      $(window).on('popstate', function () {　　　　　　　　　　　　　　　　/// 当点击浏览器的 后退和前进按钮 时才会被触发， 
+		                          window.history.pushState('forward', null, ''); 
+		                          window.history.forward(1);
+		                      });
+		                  }　　　　　　　　　　//
+		                  window.history.pushState('forward', null, '');  //在IE中必须得有这两行
+		                  window.history.forward(1);
+		              });
+	
